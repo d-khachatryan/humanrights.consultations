d05f7762ab570c18c14f97b7997488271166e6aa
davit.khachatryan <davit.khachatryan@hotmail.com>
2018-01-16 12:11:55 +0400

    The bug fixed.

--- eLConsultation.DTO/Configure.cs
+++ eLConsultation.DTO/Configure.cs
@@ -5,33 +5,33 @@ namespace eLConsultation.Data
 {
     public class Configure
     {
-        [Display(Name ="SMTP Սերվեր")]
-        [Required(ErrorMessage = "Սերվերը պարտադիր է")]
-        public string SMTP_Server { get; set; }
+        //[Display(Name ="SMTP Սերվեր")]
+        //[Required(ErrorMessage = "Սերվերը պարտադիր է")]
+        //public string SMTP_Server { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "SMTP Գաղտնաբառ")]
-        [Required(ErrorMessage = "Գաղտնաբառը պարտադիր է")]
-        public string SMTP_Password { get; set; }
+        //[DataType(DataType.Password)]
+        //[Display(Name = "SMTP Գաղտնաբառ")]
+        //[Required(ErrorMessage = "Գաղտնաբառը պարտադիր է")]
+        //public string SMTP_Password { get; set; }
 
-        [EmailAddress]
-        [Display(Name = "SMTP Էլ․ փոստի հասցե")]
-        [Required(ErrorMessage = "Էլ․ փոստի հասցեն պարտադիր է")]
-        public string SMTP_Email { get; set; }
+        //[EmailAddress]
+        //[Display(Name = "SMTP Էլ․ փոստի հասցե")]
+        //[Required(ErrorMessage = "Էլ․ փոստի հասցեն պարտադիր է")]
+        //public string SMTP_Email { get; set; }
 
-        [Display(Name = "SMTP Պորտ")]
-        [Required(ErrorMessage = "Պորտը պարտադիր է")]
-        public string SMTP_Port { get; set; }
+        //[Display(Name = "SMTP Պորտ")]
+        //[Required(ErrorMessage = "Պորտը պարտադիր է")]
+        //public string SMTP_Port { get; set; }
 
-        [Display(Name = "SMTP Առաքման մեթոդ")]
-        [Required(ErrorMessage = "Առաքման մեթոդը պարտադիր է")]
-        public int SMTP_DeliveryMethod { get; set; }
+        //[Display(Name = "SMTP Առաքման մեթոդ")]
+        //[Required(ErrorMessage = "Առաքման մեթոդը պարտադիր է")]
+        //public int SMTP_DeliveryMethod { get; set; }
 
-        [Display(Name = "Կիրառել սկզբնական լիազորությունները")]
-        public bool SMTP_UseDefaultCredentials { get; set; }
+        //[Display(Name = "Կիրառել սկզբնական լիազորությունները")]
+        //public bool SMTP_UseDefaultCredentials { get; set; }
 
-        [Display(Name = "Կիրառել SSL")]
-        public bool SMTP_EnableSsl { get; set; }
+        //[Display(Name = "Կիրառել SSL")]
+        //public bool SMTP_EnableSsl { get; set; }
 
         [Display(Name = "Գործարկող")]
         [Required(ErrorMessage = "Գործարկողը պարտադիր է")]
