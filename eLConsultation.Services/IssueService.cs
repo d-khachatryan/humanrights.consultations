9019a2c2fe4e3d1e4c121a0cc9d731f448215705
Vahram Martinyan <vahram.martinyan@hotmail.com>
2018-01-22 18:24:21 +0400

    ELC-13-Commit-4 - Delete Handle Exception

--- eLConsultation.Services/IssueService.cs
+++ eLConsultation.Services/IssueService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -324,9 +326,48 @@ namespace eLConsultation.Data
             }
             catch (Exception ex)
             {
-                exception = ex;
+                HandleException(ex);
                 return null;
             }
+        }
+
+        public virtual void HandleException(Exception exception)
+        {
+            if (exception is DbUpdateConcurrencyException concurrencyEx)
+            {
+                // A custom exception of yours for concurrency issues
+                throw new Exception(String.Format("Դուք չեք կարող հեռացնել խնդիրը, քանի որ տվյալ խնդրի շրջանակում առկա է խորհրդատվություն"));
+            }
+            else if (exception is DbUpdateException dbUpdateEx)
+            {
+                if (dbUpdateEx.InnerException != null
+                        && dbUpdateEx.InnerException.InnerException != null)
+                {
+                    if (dbUpdateEx.InnerException.InnerException is SqlException sqlException)
+                    {
+                        switch (sqlException.Number)
+                        {
+                            case 2627:  // Unique constraint error
+                            case 547:   // Constraint check violation
+                            case 2601:  // Duplicated key row error
+                                        // Constraint violation exception
+                                        // A custom exception of yours for concurrency issues
+                                throw new Exception(String.Format("Դուք չեք կարող հեռացնել խնդիրը, քանի որ տվյալ խնդրի շրջանակում առկա է խորհրդատվություն"));
+                            default:
+                                //// A custom exception of yours for other DB issues
+                                //throw new DatabaseAccessException(
+                                //dbUpdateEx.Message, dbUpdateEx.InnerException);
+                                throw new Exception(String.Format("Դուք չեք կարող հեռացնել խնդիրը, քանի որ տվյալների բազան հասանելի չէ"));
+                        }
+                    }
+
+                    //throw new DatabaseAccessException(dbUpdateEx.Message, dbUpdateEx.InnerException);
+                    throw new Exception(String.Format("Դուք չեք կարող հեռացնել խնդիրը, քանի որ տվյալների բազան հասանելի չէ"));
+                }
+            }
+
+            // If we're here then no exception has been thrown
+            // So add another piece of code below for other exceptions not yet handled...
         }
 
     }
