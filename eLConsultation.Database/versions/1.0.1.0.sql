091058731521c9c1be802ceefc64bb660b9fe9c2
davit khachatryan <davit.khachatryan@hotmail.com>
2018-02-01 08:57:36 +0400

    1.0.1.0 release

--- eLConsultation.Database/versions/1.0.1.0.sql
+++ eLConsultation.Database/versions/1.0.1.0.sql
@@ -5,3 +5,5 @@ ALTER TABLE [dbo].[TypeConsultationDeclarationType] ADD [ResponseContent] NVARCH
 ALTER TABLE [dbo].[TmpTypeConsultationDeclarationType] DROP CONSTRAINT [FK_TmpTypeConsultationDeclarationType_ResponseContent]
 ALTER TABLE [dbo].[TmpTypeConsultationDeclarationType] DROP COLUMN [ResponseContentID]
 ALTER TABLE [dbo].[TmpTypeConsultationDeclarationType] ADD [ResponseContent] NVARCHAR(100)
+
+UPDATE dbo.Setting SET SettingValue = '1.0.1.0' WHERE SettingItem = 'version'
