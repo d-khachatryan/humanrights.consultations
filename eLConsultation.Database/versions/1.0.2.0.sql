a9f6dee4270ef9e680fd27b84cb857f69b3db5fd
Vahram Martinyan <vahram.martinyan@hotmail.com>
2018-02-09 22:56:24 +0400

    ELC-20-Commit1

---
+++ eLConsultation.Database/versions/1.0.2.0.sql
@@ -0,0 +1,3 @@
+ALTER TABLE [eConsultationDB].[dbo].[Resident] ADD BirthYear SMALLINT
+ALTER TABLE [eConsultationDB].[dbo].[Resident] ADD Phone nvarchar(20)
+ALTER TABLE [eConsultationDB].[dbo].[Resident] ADD Email nvarchar(50)
