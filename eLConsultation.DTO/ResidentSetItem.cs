47b4cbe3897fa8ff6b8c4f2ff4d111ff53b2eb74
Vahram Martinyan <vahram.martinyan@hotmail.com>
2018-02-22 01:46:06 +0400

    ELC-26 DONE

--- eLConsultation.DTO/ResidentSetItem.cs
+++ eLConsultation.DTO/ResidentSetItem.cs
@@ -52,7 +52,7 @@ namespace eLConsultation.Data
         public string Home { get; set; }
 
         [Display(Name = "Ծննդյան տարեթիվ")]
-        public Int16 BirthYear { get; set; }
+        public Int16? BirthYear { get; set; }
 
         [Display(Name = "Հեռախոս")]
         public string Phone { get; set; }
